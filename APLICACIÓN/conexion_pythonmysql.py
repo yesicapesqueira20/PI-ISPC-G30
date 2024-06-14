4f75c2307294145c7fae7e36481490162ed132a3
Yesica Pesqueira <yesica.pesqueira@gmail.com>
2024-06-14 04:02:42 -0300

    #conexion con bd

--- APLICACIÓN/conexion_pythonmysql.py
+++ APLICACIÓN/conexion_pythonmysql.py
@@ -2,15 +2,19 @@ import mysql.connector
 
 class Cconexion
 
-    def ConexionBD ()
+    def ConexionBD()
         try
-            conexion = mysql.connector.connect(user='root', password='080910',
-                                               host='271.0.0.1',
-                                               database='mydb'
-                                               port='3306')
+            conexion = mysql.connector.connect(user='root', password='080910', host='271.0.0.1', database='mydb', port='3306')
+            
+             print("conexio exitosa")
+
+            return conexion
             
         
 
         except mysql.connector.Error as error
             print("error al conectar bd")
-            
+
+            return conexion
+
+    ConexionBD()
